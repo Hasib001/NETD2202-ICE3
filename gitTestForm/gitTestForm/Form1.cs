0ef718146355911a8a09f05dd5b2ab5d3026e957
S M Hasibur Rahman <hasibrahman1997@gmail.com>
2020-06-24 16:48:04 -0400

    declared a variavle

--- gitTestForm/gitTestForm/Form1.cs
+++ gitTestForm/gitTestForm/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,16 @@ namespace gitTestForm
 {
     public partial class Form1 : Form
     {
+      
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int value;
+
         }
     }
 }
